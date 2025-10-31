ce4ce593156be1310a430251addd55fb2bb09934
stompesi <stompesi@gmail.com>
2025-10-31 15:24:09 +0900

    temp

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,9 @@ use tokio::time::sleep;
 #[tokio::main]
 async fn main() {
     let client = Client::new();
-    let platform_url = "https://ethereum-holesky-rpc.publicnode.com";
-    let executor_address = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
 
-    /*
-       let rollup_id = "radius_rollup_sign";
-       let rpc_urls = [
-           "http://34.47.120.77:5000",
-           "http://34.64.46.56:5000",
-           "http://34.64.32.56:5000",
-           "http://34.64.94.33:5000",
-           "http://34.47.93.98:5000",
-       ];
-       let tx_orderer_addresses = [
-           "0x4b3dd373002d4626cf5f535c0a170f724217490c",
-           "0xd18d378823a7d2c227dfbe92f6e8fb2f1fe7b3d6",
-           "0xb4cffa6aa0062e93f387be8c441297454de7c675",
-           "0xc325eee9b01bce01c59b38440a2e1be3196327e2",
-           "0x6f3b1562efba69885a6156bb5ecde30bcf5a8a1e",
-       ];
-    */
+    let platform_url = "http://14.32.133.68:8545";
+    let executor_address = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
 
     let rollup_id = "rollup_id_2";
     let rpc_urls = [
@@ -69,73 +52,44 @@ async fn main() {
     if let Some(hex_str) = response["result"].as_str() {
         match u64::from_str_radix(hex_str.trim_start_matches("0x"), 16) {
             Ok(mut platform_block_height) => loop {
-                let leader_index = (rollup_block_height) % tx_orderer_addresses.len();
-                let next_leader_index = (leader_index + 1) % tx_orderer_addresses.len();
-
-                println!("Leader index: {}", leader_index);
-
-                let request_body = json!({
-                            "jsonrpc": "2.0",
-                            "method": "finalize_block",
-                            "params": {
-                                "finalize_block_message": {
-                                    "rollup_id": rollup_id,
-                                    "executor_address": executor_address,
-                                    "platform_block_height": platform_block_height,
-                                    "rollup_block_height": rollup_block_height,
-                                    "block_creator_address":
-                tx_orderer_addresses[leader_index],
-                "next_block_creator_address": tx_orderer_addresses[next_leader_index],
-                                },
-                                "signature": ""
-                            },
-                            "id": 1
-                        });
+                let current_leader_tx_orderer_index =
+                    (rollup_block_height) % tx_orderer_addresses.len();
+                let next_leader_tx_orderer_index =
+                    (current_leader_tx_orderer_index + 1) % tx_orderer_addresses.len();
 
                 println!(
-                    "Request platform_block_height: {} / rollup_block_height: {}",
-                    platform_block_height, rollup_block_height
+                    "Current leader tx orderer address: {}\nnext leader tx orderer address: {}",
+                    tx_orderer_addresses[current_leader_tx_orderer_index],
+                    tx_orderer_addresses[next_leader_tx_orderer_index]
                 );
 
+                let request_body = json!({
+                    "jsonrpc": "2.0",
+                    "method": "get_raw_transaction_list",
+                    "params": {
+                        "leader_change_message": {
+                            "rollup_id": rollup_id,
+                            "executor_address": executor_address,
+                            "platform_block_height": platform_block_height - 3,
+                            "current_leader_tx_orderer_address": tx_orderer_addresses[current_leader_tx_orderer_index],
+                            "next_leader_tx_orderer_address": tx_orderer_addresses[next_leader_tx_orderer_index],
+                        },
+                        "rollup_signature": "0xc6bA578acFF1eA914A6a727b2F20776eB4ad61EE333333333333333333333333c6bA578acFF1eA914A6a727b2F20776eB4ad61EE33333333333333333333333333"
+                    },
+                    "id": 1
+                });
+
                 match client
-                    .post(rpc_urls[leader_index])
+                    .post(rpc_urls[current_leader_tx_orderer_index])
                     .json(&request_body)
                     .send()
                     .await
                 {
                     Ok(response) => {
-                        let request_body = json!({
-                            "jsonrpc": "2.0",
-                            "method": "get_raw_transaction_list",
-                            "params": {
-                                "rollup_id": rollup_id,
-                                "rollup_block_height": rollup_block_height,
-                            },
-                            "id": 1
-                        });
+                        let response = response.json::<serde_json::Value>().await.unwrap();
 
+                        println!("Response {:?}\n", response);
                         rollup_block_height += 1;
-
-                        loop {
-                            match client
-                                .post(rpc_urls[rollup_block_height % rpc_urls.len()])
-                                .json(&request_body)
-                                .send()
-                                .await
-                            {
-                                Ok(response) => {
-                                    let response =
-                                        response.json::<serde_json::Value>().await.unwrap();
-
-                                    if response["error"].is_null() {
-                                        break;
-                                    } else {
-                                        eprintln!("Request failed: {:?}", response);
-                                    }
-                                }
-                                Err(e) => eprintln!("Request failed: {}", e),
-                            }
-                        }
                     }
                     Err(e) => eprintln!("Request failed: {}", e),
                 }
